0483eca665ff5eb3d13d5b964e365d794ece12e2
Fernando Corrales <corrales_fernando@hotmail.com>
2025-03-09 12:44:14 -0300

    add run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+if __name__ == "__main__":  # 🔹 Importante para evitar problemas en Windows
+    uvicorn.run(
+        "src.main:app", host="127.0.0.1", port=8000, reload=False, loop="asyncio"
+    )
+
+# uvicorn src.main:app --loop asyncio
