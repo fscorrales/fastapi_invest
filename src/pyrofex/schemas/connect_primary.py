1eea00f120d79df3dd9707f08cd79df85cdb48e3
Fernando Corrales <fscorrales@gmail.com>
2025-04-21 16:00:14 -0300

    fix: update websocket_url field to be optional in ConnectPrimary model

--- src/pyrofex/schemas/connect_primary.py
+++ src/pyrofex/schemas/connect_primary.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 # --------------------------------------------------
 class ConnectPrimary(BaseModel):
     base_url: str
-    websocket_url: str
+    websocket_url: Optional[str] = None
     server: Optional[str] = None
     date: datetime
     content_length: str
