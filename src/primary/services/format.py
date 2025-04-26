c8fe90582cc23874612bbe6066d23fe506c787ca
Fernando Corrales <fscorrales@gmail.com>
2025-04-26 20:12:02 -0300

    feat: update format_instruments to accept a single symbol as input

--- src/primary/services/format.py
+++ src/primary/services/format.py
@@ -7,10 +7,13 @@ from ..schemas import SettlementTerm
 
 # --------------------------------------------------
 def format_instruments(
-    symbols: List[str],
+    symbols: Union[List[str], str],
     settlement_terms: Union[List[SettlementTerm], SettlementTerm] = ["CI", "24hs"],
 ) -> List[str]:
     """Get formatted instruments from symbols"""
+    if not isinstance(symbols, list):
+        # Si symbols es un único símbolo, lo convertimos en una lista
+        symbols = [symbols]
     if not isinstance(settlement_terms, list):
         # Si settlement_terms es un único SettlementTerm, lo convertimos en una lista
         settlement_terms = [settlement_terms]
