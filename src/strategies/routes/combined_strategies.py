75385d07c2f5665482a55f0cf9c37ec838d35541
Fernando Corrales <fscorrales@gmail.com>
2025-05-28 16:27:18 -0300

    fix: refactor start_all function to include upload_to_google_sheets parameter and improve logging

--- src/strategies/routes/combined_strategies.py
+++ src/strategies/routes/combined_strategies.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, Query
 
 from ...auth.services import OptionalAuthorizationDependency
 from ...config import logger
@@ -13,13 +13,12 @@ from ...primary.services import (
     WSMarketDataServiceDependency,
     prepare_primary_credentials,
 )
-
 from ..services import (
-    strategy_manager,
-    TimeArbitrageService,
-    OptionCoberedCallService,
-    TIME_ARBITRAGE_NAME,
     OPTION_COBERED_CALL_NAME,
+    TIME_ARBITRAGE_NAME,
+    OptionCoberedCallService,
+    TimeArbitrageService,
+    strategy_manager,
 )
 
 STRATEGY_NAME = "combined_strategies"
@@ -35,26 +34,44 @@ async def start_all(
     service: WSMarketDataServiceDependency,
     credentials: Annotated[PrimaryCredentials, Depends()],
     days: int = 1,
+    upload_to_google_sheets: bool = Query(False, alias="uploadToGoogleSheets"),
 ):
-
     credentials = prepare_primary_credentials(auth, credentials)
 
     logger.info(
         f"Syncing {credentials.enviroment.value} instruments details from Primary API with url: {credentials.url}"
     )
 
-    strategy = TimeArbitrageService(market_data_service=service, days=days)
+    strategy = TimeArbitrageService(
+        market_data_service=service,
+        days=days,
+        upload_to_google_sheets=upload_to_google_sheets,
+    )
+    if TIME_ARBITRAGE_NAME in strategy_manager.list_active():
+        logger.warning("La estrategia de arbitraje de tiempo ya está corriendo")
+    else:
+        logger.info("Registrando la estrategia de arbitraje de tiempo")
     strategy_manager.register(TIME_ARBITRAGE_NAME, strategy)
-    strategy = OptionCoberedCallService(market_data_service=service, days=days)
+    strategy = OptionCoberedCallService(
+        market_data_service=service,
+        days=days,
+        upload_to_google_sheets=upload_to_google_sheets,
+    )
+    if OPTION_COBERED_CALL_NAME in strategy_manager.list_active():
+        logger.warning("La estrategia de opción cubierta ya está corriendo")
+    else:
+        logger.info("Registrando la estrategia de opción cubierta")
     strategy_manager.register(OPTION_COBERED_CALL_NAME, strategy)
 
     await strategy_manager.start_all(credentials)
     return {"status": "Todas las estrategias iniciadas"}
 
+
 @combined_strategies_router.post("/stop_all")
 async def stop_all():
     strategy_manager.stop_all()
     return {"status": "Todas las estrategias detenidas"}
+
 
 @combined_strategies_router.get("/status")
 def list_running():
