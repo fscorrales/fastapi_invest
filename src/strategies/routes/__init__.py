9221f1ab95a98212e28a00c0d3112786dcf0c735
Fernando Corrales <fscorrales@gmail.com>
2025-06-03 16:26:21 -0300

    fix: reorder imports in __init__.py for consistency and clarity

--- src/strategies/routes/__init__.py
+++ src/strategies/routes/__init__.py
@@ -2,12 +2,14 @@ __all__ = ["strategies_router"]
 
 from fastapi import APIRouter
 
-from .time_arbitrage import time_arbitrage_router
-from .option_cobered_call import option_cobered_call_router
 from .combined_strategies import combined_strategies_router
+from .option_cobered_call import option_cobered_call_router
+from .option_necklace import option_necklace_router
+from .time_arbitrage import time_arbitrage_router
 
 strategies_router = APIRouter(prefix="/strategies")
 
 strategies_router.include_router(combined_strategies_router)
 strategies_router.include_router(time_arbitrage_router)
 strategies_router.include_router(option_cobered_call_router)
+strategies_router.include_router(option_necklace_router)
