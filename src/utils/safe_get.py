7d6ff988e6afca8a8869a90ad9f0af3a0bd45c22
Fernando Corrales <fscorrales@gmail.com>
2025-06-04 20:08:08 -0300

    refactor: enhance sanitize_dataframe_for_json to improve NaN handling and type inference

--- src/utils/safe_get.py
+++ src/utils/safe_get.py
@@ -45,21 +45,24 @@ def sanitize_dataframe_for_json(df: pd.DataFrame) -> pd.DataFrame:
     - np.nan, np.inf, -np.inf con None
     - np.* types con sus equivalentes nativos
     """
-    # Reemplazar NaN e infinitos por None
-    # df_clean = df.replace([np.nan, np.inf, -np.inf], None)
-    df_clean = df.replace([np.nan, np.inf, -np.inf, None], "")
-    df_clean = df_clean.astype(object)
+    with pd.option_context("future.no_silent_downcasting", True):
+        # Reemplazar NaN e infinitos por None
+        # df_clean = df.replace([np.nan, np.inf, -np.inf], None)
+        df_clean = df.replace([np.nan, np.inf, -np.inf, None], "").infer_objects(
+            copy=False
+        )
+        df_clean = df_clean.astype(object).where(pd.notnull(df_clean), None)
+        # df_clean = df_clean.astype(object)
 
-    # Convertir a object donde haya valores nulos para asegurar compatibilidad
-    # df_clean = df_clean.astype(object).where(pd.notnull(df_clean), None)
+        # Convertir a object donde haya valores nulos para asegurar compatibilidad
 
-    # Convertir np.* types (como np.int64, np.float64) a sus tipos nativos
-    df_clean = df_clean.apply(
-        lambda col: col.map(lambda x: x.item() if hasattr(x, "item") else x)
-    )
-    # df_clean = df_clean.applymap(lambda x: x.item() if hasattr(x, "item") else x)
+        # Convertir np.* types (como np.int64, np.float64) a sus tipos nativos
+        df_clean = df_clean.apply(
+            lambda col: col.map(lambda x: x.item() if hasattr(x, "item") else x)
+        )
+        # df_clean = df_clean.applymap(lambda x: x.item() if hasattr(x, "item") else x)
 
-    # Convertir todo a string para evitar problemas con tipos
-    # df_clean = df_clean.astype(str)
+        # Convertir todo a string para evitar problemas con tipos
+        # df_clean = df_clean.astype(str)
 
-    return df_clean
+        return df_clean
