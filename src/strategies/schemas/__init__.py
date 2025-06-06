d8474c8ad5164d016fca5922820f195c5f4d97f6
Fernando Corrales <fscorrales@gmail.com>
2025-06-06 16:12:33 -0300

    feat: add OptionBullSpreadSummary and OptionBullSpreadFilter classes

--- src/strategies/schemas/__init__.py
+++ src/strategies/schemas/__init__.py
@@ -1,4 +1,5 @@
 from .fees import *
+from .option_bull_spread import *
 from .option_cobered_call import *
 from .option_necklace import *
 from .time_arbitrage import *
---
+++ src/strategies/schemas/option_bull_spread.py
@@ -0,0 +1,44 @@
+__all__ = ["OptionBullSpreadSummary", "OptionBullSpreadFilter"]
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ...utils import BaseFilterParams
+
+
+# --------------------------------------------------
+class OptionBullSpreadSummary(BaseModel):
+    underlying_ticker: str
+    type: str
+    days_expire: int
+    ticker_x0: str
+    ticker_x1: str
+    tna_max_profit: float
+    min_invest: float
+    tna_max_diff: float
+    var_max_loss: float
+    var_pe: float
+    var_max_profit: float
+    spread_pct: float
+    strike_x0: float
+    strike_x1: float
+    prima_x0: float
+    size_x0: float
+    size_x1: float
+    prima_x1: float
+    underlying_close: float
+    prima_neta: float
+    spread: float
+    pe: float
+    max_profit: float
+    max_profit_pct: float
+    max_loss: float
+    max_loss_pct: float
+    tna_max_loss: float
+
+
+# --------------------------------------------------
+class OptionBullSpreadFilter(BaseFilterParams):
+    ticker_x0: Optional[str] = None
+    ticker_x1: Optional[str] = None
