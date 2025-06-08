12d588fc87177777cba793987221f66677e9b1df
Fernando Corrales <fscorrales@gmail.com>
2025-06-08 20:22:31 -0300

    feat: add option_bull_spread router for option trading strategy

--- src/strategies/routes/__init__.py
+++ src/strategies/routes/__init__.py
@@ -3,6 +3,7 @@ __all__ = ["strategies_router"]
 from fastapi import APIRouter
 
 from .combined_strategies import combined_strategies_router
+from .option_bull_spread import option_bull_spread_router
 from .option_cobered_call import option_cobered_call_router
 from .option_necklace import option_necklace_router
 from .time_arbitrage import time_arbitrage_router
@@ -13,3 +14,4 @@ strategies_router.include_router(combined_strategies_router)
 strategies_router.include_router(time_arbitrage_router)
 strategies_router.include_router(option_cobered_call_router)
 strategies_router.include_router(option_necklace_router)
+strategies_router.include_router(option_bull_spread_router)
