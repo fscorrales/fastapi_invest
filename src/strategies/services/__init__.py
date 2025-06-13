89275e15d77048932e88d2a31b4e94a60f85af7f
Fernando Corrales <fscorrales@gmail.com>
2025-06-13 19:41:29 -0300

    feat: add OptionCallRatioBackspreadService and update __init__.py imports

--- src/strategies/services/__init__.py
+++ src/strategies/services/__init__.py
@@ -1,6 +1,7 @@
 from .base_strategy import *
 from .option_bear_spread import *
 from .option_bull_spread import *
+from .option_call_ratio_backspread import *
 from .option_cobered_call import *
 from .option_necklace import *
 from .strategy_manager import *
