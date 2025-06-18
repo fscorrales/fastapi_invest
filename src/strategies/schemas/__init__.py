d2c31fbbdddd40f4800c70e918c776a8484cd78d
Fernando Corrales <fscorrales@gmail.com>
2025-06-18 16:34:41 -0300

    feat: add OptionMarketDataSummary and OptionMarketDataFilter classes for market data representation

--- src/strategies/schemas/__init__.py
+++ src/strategies/schemas/__init__.py
@@ -1,5 +1,6 @@
 from .fees import *
 from .option_cobered_call import *
+from .option_market_data import *
 from .option_necklace import *
 from .option_ratio_backspread import *
 from .option_spread import *
---
+++ src/strategies/schemas/option_market_data.py
@@ -0,0 +1,33 @@
+__all__ = ["OptionMarketDataSummary", "OptionMarketDataFilter"]
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from ...utils import BaseFilterParams
+
+
+# --------------------------------------------------
+class OptionMarketDataSummary(BaseModel):
+    ticker: str
+    bid_size: Optional[float] = None
+    bid: Optional[float] = None
+    last: Optional[float] = None
+    ask: Optional[float] = None
+    ask_size: Optional[float] = None
+    open: Optional[float] = None
+    high: Optional[float] = None
+    low: Optional[float] = None
+    close: Optional[float] = None
+    chg_pct: Optional[float] = None
+    volume: Optional[float] = None
+    nom_volumne: Optional[float] = None
+    underlying_ticker: str
+    days_expire: int
+    strike: float
+    underlying_close: float
+
+
+# --------------------------------------------------
+class OptionMarketDataFilter(BaseFilterParams):
+    ticker: Optional[str] = None
