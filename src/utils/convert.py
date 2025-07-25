a09e7f17d7285469b87ef2354e8e15c92290e462
Fernando Corrales <fscorrales@gmail.com>
2025-07-25 12:47:42 -0300

    add convert_str_column_to_datetime_safe and convert_str_to_date_only_safe to utils.convert

--- src/utils/convert.py
+++ src/utils/convert.py
@@ -1,7 +1,14 @@
-__all__ = ["to_float", "parse_date"]
+__all__ = [
+    "to_float",
+    "parse_date",
+    "convert_str_column_to_datetime_safe",
+    "convert_str_to_date_only_safe",
+]
 
 from datetime import date, datetime
 from typing import Optional
+
+import pandas as pd
 
 
 # --------------------------------------------------
@@ -18,3 +25,17 @@ def to_float(value: str, default: Optional[float] = None) -> Optional[float]:
 # --------------------------------------------------
 def parse_date(date_str: str, fmt: str = "%d/%m/%Y") -> date:
     return datetime.strptime(date_str, fmt).date()
+
+
+# --------------------------------------------------
+def convert_str_column_to_datetime_safe(col: pd.Series, fmt: str = None) -> pd.Series:
+    return pd.to_datetime(col, errors="coerce", format=fmt).apply(
+        lambda x: x.to_pydatetime() if pd.notnull(x) else None
+    )
+
+
+# --------------------------------------------------
+def convert_str_to_date_only_safe(col: pd.Series, fmt: str = None) -> pd.Series:
+    return pd.to_datetime(col, errors="coerce", format=fmt).apply(
+        lambda x: x.date() if pd.notnull(x) else None
+    )
